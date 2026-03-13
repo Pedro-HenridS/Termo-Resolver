a1235407799ba5ea5343f27dedf9ce3e04bad901
Pedro-HenridS <pedrosouza9479@gmail.com>
2026-03-13 10:11:18 -0300

    Criando interface para tentativa do termo

--- Functions/VerifyAnswer.py
+++ Functions/VerifyAnswer.py
@@ -1,21 +1,48 @@
 from collections import Counter
 
-def VerifyAnswer(word, dayWord):
+def VerifyAnswer(word: str, dayWord: str):
+
+    """
+    Verifica uma tentativa de palavra no jogo Termo.
+
+    Args:
+        word (str): Palavra tentada pelo jogador.
+        dayWord (str): Palavra correta do dia.
+
+    Returns:
+        tuple:
+            correctSpace (list):
+                Lista indicando o resultado visual de cada letra
+                ("🟩", "🟨", "🟥").
+
+            correctSpaceNumeric (list):
+                Lista numérica com o resultado de cada letra
+                0  = letra incorreta
+                1  = letra correta
+                -1 = letra correta na posição errada
+
+            wrongLetters (list):
+                Lista de letras que não existem na palavra do dia.
+    """
     
-    correctSpace = [None] * len(word)
+    viewResult = [None] * len(word)
+    guessVerify = [None] * len(word)
     wrongLetters = []
     dayWordLettersCount = Counter(dayWord)
 
     for i, (letter, dayLetter) in enumerate(zip(word, dayWord)):
         if letter == dayLetter:
-            correctSpace[i] = "✅ " + letter
+            viewResult[i] = "✅ " + letter
+            guessVerify[i] = 1
 
         elif letter != dayLetter and letter in dayWord and dayWordLettersCount[letter] > 0:
-            correctSpace[i] = "🟨 " + letter
+            viewResult[i] = "🟨 " + letter
+            guessVerify[i] = -1
             dayWordLettersCount[letter] -= 1
 
         else:
-            correctSpace[i] = "🟥 " + letter
-            wrongLetters.append(correctSpace[i])
+            viewResult[i] = "🟥 " + letter
+            guessVerify[i] = 0
+            wrongLetters.append(letter)
 
-    return (correctSpace, wrongLetters)
+    return (viewResult, guessVerify, wrongLetters)
--- Main.py
+++ Main.py
@@ -1,13 +1,69 @@
 from Functions.VerifyAnswer import VerifyAnswer
+from Functions.FilterCandidates import FilterCandidatesByWrongLetters
 
 def main():
 
-    attempt = input("Insira sua tentativa [5 letras]: ")
+    candidates = [
+    "carta",
+    "piano",
+    "verde",
+    "livro",
+    "nuvem",
+    "canto",
+    "prato",
+    "lente",
+    "pedra",
+    "campo",
+    "festa",
+    "corte",
+    "poder",
+    "viver",
+    "luzir",
+    "brisa",
+    "folha",
+    "areia",
+    "caixa",
+    "pauta",
+    "sonho",
+    "claro",
+    "pular",
+    "beijo",
+    "magia",
+    "tempo",
+    "risco",
+    "linha",
+    "pilar",
+    "vento",
+    "praia"
+]
     
-    template, wrongLetters = VerifyAnswer(attempt, "praia")
+    guesses = {}
+    viwwGuesses = {}
+    allWrongLetters = []
 
-    print(template)
-    print(wrongLetters)
+    for i in range(5):
+    
+        guess = input("Insira sua tentativa [5 letras]: ")
+        viewTemplate, template, wrongLetters = VerifyAnswer(guess, "praia")
+
+        guesses[i] = template
+        viwwGuesses[i] = viewTemplate
+
+        # Adiciona as letras erradas da analise atual a lista completa de letras erradas 
+        allWrongLetters.extend(x for x in wrongLetters if x not in allWrongLetters)
+
+        for i in range(len(viwwGuesses)):
+            print(*viwwGuesses[i])
+
+        candidates = FilterCandidatesByWrongLetters(candidates, allWrongLetters)
+
+        for word in candidates:
+            print(word)    
+
+    
+        
+    for i, letter in enumerate(allWrongLetters):
+            print(letter, end=" ")
 
     return 0
 
